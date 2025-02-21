e9b0459e7b06846ca1b23dc84b7d84602970decc
Azhar020606 <maulanaazhar0206@gmail.com>
2025-02-21 10:56:17 +0700

    Add files via upload

---
+++ web-2-backup/Variable_konstanta.php
@@ -0,0 +1,9 @@
+<?php
+define ('PHI', 3.14);
+$jari_jari = 100;
+
+$luas = PHI * $jari_jari * $jari_jari;
+
+echo 'nilai phi adalah ' . PHI;
+echo '<br/> luas lingkaran dengan jari jari 100 adalah' . $jari_jari ;
+?> 
---
+++ web-2-backup/Variable_user.php
@@ -0,0 +1,11 @@
+<?php
+$nama = 'azhar maulana';
+$umur = 18;
+$berat = 92;
+
+echo 'nama saya adalah :' .$nama;
+echo '<br/>umur ' . $umur . ' tahun';
+echo '<br/> berat : ' . $berat . ' kg';
+
+echo "<br/>hello apa $nama apa kabar?";
+?>
