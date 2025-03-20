1c23d1eaab748552356b74cc3fc0a7e26e18f7a2
Azhar020606 <maulanaazhar0206>
2025-03-20 11:46:28 +0700

    praktikum 4

--- README.md
+++
@@ -1,6 +0,0 @@
-# Pemograman Web 2
-
-Repositori Pratikum Mata Kuliah Pemograman Web 2
-Nama : Azhar Maulana Achmad
-Nim : 0110124210
-Rombel :SI12
--- Variable_konstanta.php
+++
@@ -1,9 +0,0 @@
-<?php
-define ('PHI', 3.14);
-$jari_jari = 100;
-
-$luas = PHI * $jari_jari * $jari_jari;
-
-echo 'nilai phi adalah ' . PHI;
-echo '<br/> luas lingkaran dengan jari jari 100 adalah' . $jari_jari ;
-?> 
